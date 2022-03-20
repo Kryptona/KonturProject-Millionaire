dadeae3e9389bbddc2c88c3043f87e2985cbeb3d
Дарья <dasha.seredenko.1997@gmail.com>
2022-03-20 22:20:54 +0400

    Добавлен react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import {Routes} from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from "react-router-dom";
+import {GamePage} from "./components/game/GamePage";
+import {MenuPage} from "./components/menu/MenuPage";
 
 const App = () => {
-  return (
-      <div>
-        <h1>Ahahah</h1>
-      </div>
-  )
+    return (
+        <div>
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Menu</Link>
+                            </li>
+                            <li>
+                                <Link to="/game">Game</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Routes>
+                        <Route index element={<MenuPage/>}/>
+                        <Route path="/game" element={<GamePage/>}/>
+                    </Routes>
+                </div>
+            </Router>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/components/menu/MenuPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const MenuPage = () => {
+    return (
+        <div>
+            MenuPage
+        </div>
+    );
+};
