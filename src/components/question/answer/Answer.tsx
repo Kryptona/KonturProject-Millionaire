fff9e2b3ecf1478f4b30c398eda910ca6264d177
MichailNonProgramer <chernogorov_m@mail.ru>
2022-03-21 14:01:53 +0500

    Add architecture answers

---
+++ src/components/question/answer/Answer.tsx
@@ -0,0 +1,10 @@
+import questions from "../../resources/questions.json"
+import React from "react";
+
+const Answer = () => {
+    return (
+        <div>Ответ</div>
+    );
+};
+
+export default Answer
