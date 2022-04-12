11bf4c39cd4d12bab7b650eb7dd6bf9004641e09
Дарья <dasha.seredenko.1997@gmail.com>
2022-04-12 17:19:09 +0400

    Добавлена firebaseDB

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import {GamePage} from './components/game/GamePage';
 import {MenuPage} from './components/menu/MenuPage';
 import {Leaderboard} from './components/leaderboard/Leaderboard';
+import {initializeApp} from 'firebase/app';
 
 const App = () => {
   return (
@@ -21,3 +22,16 @@ const App = () => {
 };
 
 export default App;
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA86RnWdjYsCGLhkNupfrmf6M5bnFup6jg',
+  authDomain: 'shpora-millionaire.firebaseapp.com',
+  databaseURL: 'https://shpora-millionaire-default-rtdb.firebaseio.com/',
+  projectId: 'shpora-millionaire',
+  storageBucket: 'shpora-millionaire.appspot.com',
+  messagingSenderId: '1096188787032',
+  appId: '1:1096188787032:web:c61bbd358772e4eb638d2d',
+  measurementId: 'G-Q8BFS1LTTT',
+};
+const app = initializeApp(firebaseConfig);
