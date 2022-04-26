b15a530767896ee3a00ba2835664fdce39e571db
Дарья <dasha.seredenko.1997@gmail.com>
2022-04-26 16:38:53 +0400

    progress bar для таймера

--- src/components/game/Timer/Timer.tsx
+++ src/components/game/Timer/Timer.tsx
@@ -1,5 +1,6 @@
 import styles from './Timer.scss';
 import React, {Dispatch, SetStateAction, useEffect} from 'react';
+import {buildStyles, CircularProgressbar} from 'react-circular-progressbar';
 
 interface PropsTimer {
   time: number;
@@ -24,8 +25,49 @@ export const Timer: React.FC<PropsTimer> = ({time, setOpenModal, setCounter, isO
   }, [time]);
 
   return (
-    <div className={styles.root}>
-      <div className={styles.time}>{time}</div>
+    <div>
+      <CircularProgressbar
+        value={time}
+        text={time + ''}
+        maxValue={30}
+        styles={{
+          // Customize the root svg element
+          root: {},
+          // Customize the path, i.e. the "completed progress"
+          path: {
+            // Path color
+            stroke: `rgba(249, 249, 0, ${time})`,
+            // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+            strokeLinecap: 'butt',
+            strokeWidth: '4',
+            // Customize transition animation
+            transition: 'stroke-dashoffset 0.5s ease 0s',
+            // Rotate the path
+            transform: 'rotate(0.0turn)',
+            transformOrigin: 'center center',
+          },
+          // Customize the circle behind the path, i.e. the "total progress"
+          trail: {
+            // Trail color
+            stroke: '#6600ff',
+            // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+            strokeLinecap: 'butt',
+            // Rotate the trail
+            transform: 'rotate(0.25turn)',
+            transformOrigin: 'center center',
+          },
+          // Customize the text
+          text: {
+            dominantBaseline: 'central',
+            textAnchor: 'middle',
+            // Text color
+            fill: '#fff300',
+            // Text size
+            fontSize: '50px',
+          },
+        }}
+        className={styles.root}
+      />
     </div>
   );
 };
